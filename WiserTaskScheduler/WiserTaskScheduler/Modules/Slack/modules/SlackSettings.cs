edc965d0872758c57b1737bb927bb101dbd7f6bd
Thimo van 't Hooft <thimovanthooft@happyhorizon.com>
2023-01-13 09:49:54 +0100

    Sent a message to slackChannel

---
+++ WiserTaskScheduler/WiserTaskScheduler/Modules/Slack/modules/SlackSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace WiserTaskScheduler.Modules.Slack.modules;
+
+public class SlackSettings
+{ 
+  /// <summary>
+  /// GET SLACK CHANNEL NAME
+  /// </summary>
+  public  string SlackChannel { get; set; }
+
+}
+
