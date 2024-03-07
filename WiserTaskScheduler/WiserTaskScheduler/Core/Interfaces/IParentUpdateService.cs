f6a754f4ab7b003966fb94f1475bd400d6d93927
Mike van Mourik <130973326+MikeVanMourik@users.noreply.github.com>
2024-03-07 14:18:55 +0100

    add parentsupdateservice to WTS

---
+++ WiserTaskScheduler/WiserTaskScheduler/Core/Interfaces/IParentUpdateService.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using WiserTaskScheduler.Core.Models;
+
+namespace WiserTaskScheduler.Core.Interfaces
+{
+    /// <summary>
+    /// A service to perform updates to parents whose child items got updated.
+    /// </summary>
+    public interface IParentUpdateService
+    {
+        /// <summary>
+        /// Gets or sets the log settings that the Main service needs to use.
+        /// </summary>
+        LogSettings LogSettings { get; set; }
+
+        /// <summary>
+        /// checks the parent update table and apply updates where needed then clear it
+        /// </summary>
+        /// <returns></returns>
+        Task ParentsUpdateAsync();
+    }
+}
