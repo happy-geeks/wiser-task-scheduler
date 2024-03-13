bf8ed0eee31d1a54d4122e32332dcd3433bb8279
Mike van Mourik <130973326+MikeVanMourik@users.noreply.github.com>
2024-03-13 08:59:33 +0100

    capital fix in comment

--- WiserTaskScheduler/WiserTaskScheduler/Core/Interfaces/IParentUpdateService.cs
+++ WiserTaskScheduler/WiserTaskScheduler/Core/Interfaces/IParentUpdateService.cs
@@ -14,7 +14,7 @@ namespace WiserTaskScheduler.Core.Interfaces
         LogSettings LogSettings { get; set; }
 
         /// <summary>
-        /// checks the parent update table and applies updates where needed then clear it.
+        /// Checks the parent update table and applies updates where needed then clear it.
         /// </summary>
         /// <returns></returns>
         Task ParentsUpdateAsync();
