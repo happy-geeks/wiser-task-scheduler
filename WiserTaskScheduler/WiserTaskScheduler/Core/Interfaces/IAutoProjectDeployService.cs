07b970a78f85629e6998e483c0aad78e9a6f2459
Mark van Dijk <markvandijk@happyhorizon.com>
2024-11-20 15:56:22 +0100

    Initial setup for the auto project deploy flow.

---
+++ WiserTaskScheduler/WiserTaskScheduler/Core/Interfaces/IAutoProjectDeployService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using WiserTaskScheduler.Core.Models;
+
+namespace WiserTaskScheduler.Core.Interfaces;
+
+public interface IAutoProjectDeployService
+{
+    /// <summary>
+    /// Gets or sets the log settings that the Auto Project Deploy service needs to use.
+    /// </summary>
+    LogSettings LogSettings { get; set; }
+
+    Task ManageAutoProjectDeploy();
+}
---
+++ WiserTaskScheduler/WiserTaskScheduler/Core/Workers/AutoProjectDeployWorker.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using WiserTaskScheduler.Core.Interfaces;
+using WiserTaskScheduler.Core.Models;
+using WiserTaskScheduler.Modules.RunSchemes.Models;
+
+namespace WiserTaskScheduler.Core.Workers;
+
+public class AutoProjectDeployWorker : BaseWorker
+{
+    private const string LogName = "AutoProjectDeploy";
+    
+    private readonly IAutoProjectDeployService autoProjectDeployService;
+    private readonly ILogService logService;
+    private readonly ILogger<AutoProjectDeployWorker> logger;
+    
+    public AutoProjectDeployWorker(IOptions<WtsSettings> wtsSettings, IAutoProjectDeployService autoProjectDeployService, ILogService logService, ILogger<AutoProjectDeployWorker> logger, IBaseWorkerDependencyAggregate baseWorkerDependencyAggregate) : base(baseWorkerDependencyAggregate)
+    {
+        // TODO: Initialize with the run scheme from the settings.
+        Initialize(LogName, new RunSchemeModel(), wtsSettings.Value.ServiceFailedNotificationEmails, true);
+        RunScheme.LogSettings ??= new LogSettings();
+        
+        this.autoProjectDeployService = autoProjectDeployService;
+        this.logService = logService;
+        this.logger = logger;
+        
+        this.autoProjectDeployService.LogSettings = RunScheme.LogSettings;
+    }
+    
+    /// <inheritdoc />
+    protected override async Task ExecuteActionAsync()
+    {
+        await autoProjectDeployService.ManageAutoProjectDeploy();
+    }
+}
