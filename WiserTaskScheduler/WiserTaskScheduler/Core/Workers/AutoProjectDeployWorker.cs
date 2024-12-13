d963ba39ea1af092b74e9fc9a2d971a565c02bad
Mark van Dijk <markvandijk@happyhorizon.com>
2024-12-13 15:10:41 +0100

    Refactoring

--- WiserTaskScheduler/WiserTaskScheduler/Core/Workers/AutoProjectDeployWorker.cs
+++ WiserTaskScheduler/WiserTaskScheduler/Core/Workers/AutoProjectDeployWorker.cs
@@ -1,9 +1,7 @@
 ﻿using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using WiserTaskScheduler.Core.Interfaces;
 using WiserTaskScheduler.Core.Models;
-using WiserTaskScheduler.Modules.RunSchemes.Models;
 
 namespace WiserTaskScheduler.Core.Workers;
 
@@ -12,18 +10,13 @@ public class AutoProjectDeployWorker : BaseWorker
     private const string LogName = "AutoProjectDeploy";
 
     private readonly IAutoProjectDeployService autoProjectDeployService;
-    private readonly ILogService logService;
-    private readonly ILogger<AutoProjectDeployWorker> logger;
 
-    public AutoProjectDeployWorker(IOptions<WtsSettings> wtsSettings, IAutoProjectDeployService autoProjectDeployService, ILogService logService, ILogger<AutoProjectDeployWorker> logger, IBaseWorkerDependencyAggregate baseWorkerDependencyAggregate) : base(baseWorkerDependencyAggregate)
+    public AutoProjectDeployWorker(IOptions<WtsSettings> wtsSettings, IAutoProjectDeployService autoProjectDeployService, IBaseWorkerDependencyAggregate baseWorkerDependencyAggregate) : base(baseWorkerDependencyAggregate)
     {
         Initialize(LogName, wtsSettings.Value.AutoProjectDeploy.RunScheme, wtsSettings.Value.ServiceFailedNotificationEmails, true);
         RunScheme.LogSettings ??= new LogSettings();
 
         this.autoProjectDeployService = autoProjectDeployService;
-        this.logService = logService;
-        this.logger = logger;
-
         this.autoProjectDeployService.LogSettings = RunScheme.LogSettings;
     }
 
