3e7f045f82528c9dd7594ce66608d8bd22e64484
MarkVanDijkHG <89684687+MarkVanDijkHG@users.noreply.github.com>
2025-03-27 14:13:08 +0100

    Extend ResultSetHelper tests.

--- WiserTaskScheduler/WiserTaskScheduler.Tests/Core/Helpers/ResultSetHelperTests.cs
+++ WiserTaskScheduler/WiserTaskScheduler.Tests/Core/Helpers/ResultSetHelperTests.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using Newtonsoft.Json.Linq;
 using WiserTaskScheduler.Core.Helpers;
+using WiserTaskScheduler.Core.Models;
 
 namespace WiserTaskScheduler.Tests.Core.Helpers;
 
@@ -13,21 +14,51 @@ public class ResultSetHelperTests
     {
         resultSet = new JObject
         {
-            ["Value"] = "Test",
+            ["Value"] = "Test1",
             ["Body"] = new JObject
             {
-                ["Value"] = "Test"
+                ["Value"] = "Test2",
+                ["Array"] = new JArray
+                {
+                    new JValue("Index0"),
+                    new JValue("Index1"),
+                    new JValue("Index2")
+                },
+                ["ObjectArray"] = new JArray
+                {
+                    new JObject
+                    {
+                        ["Value"] = "Test3",
+                        ["Array"] = new JArray
+                        {
+                            new JValue("InnerArrayIndex0"),
+                            new JValue("InnerArrayIndex1"),
+                            new JValue("InnerArrayIndex2")
+                        }
+                    },
+                    new JObject
+                    {
+                        ["Value"] = "Test4"
+                    }
+                }
             }
         };
     }
 
     [Test]
-    [TestCase("Value", "Test")]
-    [TestCase("Body.Value", "Test")]
-    public void GetCorrectObject_BasicPath_ReturnsCorrectObject(string key, string expectedValue)
+    [TestCase("Value", "Test1")]
+    [TestCase("Body.Value", "Test2")]
+    [TestCase("Body.Array[0]", "Index0")]
+    [TestCase("Body.Array[1]", "Index1")]
+    [TestCase("Body.Array[2]", "Index2")]
+    [TestCase("Body.Array[i]", "Index1", 1, 0)]
+    [TestCase("Body.ObjectArray[i].Value", "Test3", 0, 0)]
+    [TestCase("Body.ObjectArray[i].Array[0]", "InnerArrayIndex0", 0, 0)]
+    [TestCase("Body.ObjectArray[i].Array[j]", "InnerArrayIndex1", 0, 1)]
+    public void GetCorrectObject_ValidPath_ReturnsCorrectObject(string key, string expectedValue, int indexI = 0, int indexJ = 0)
     {
         // Act
-        var actual = ResultSetHelper.GetCorrectObject<JValue>(key, null, resultSet);
+        var actual = ResultSetHelper.GetCorrectObject<JValue>(key, [indexI, indexJ], resultSet);
 
         // Assert
         actual.Should<JValue>().NotBeNull("because we are only testing values present in the result set.");
@@ -35,9 +66,10 @@ public class ResultSetHelperTests
     }
 
     [Test]
+    [TestCase("")]
     [TestCase("Invalid")]
     [TestCase("Body.Invalid")]
-    public void GetCorrectObject_InvalidBasicPath_ReturnsNull(string key)
+    public void GetCorrectObject_InvalidPath_ReturnsNull(string key)
     {
         // Act
         var actual = ResultSetHelper.GetCorrectObject<JValue>(key, null, resultSet);
@@ -49,12 +81,16 @@ public class ResultSetHelperTests
     [Test]
     [TestCase("Invalid.Value")]
     [TestCase("Body.Invalid.Value")]
-    public void GetCorrectObject_InvalidBasicPath_ThrowsException(string key)
+    [TestCase("Body.Array[3]")]
+    [TestCase("Body.Array[i]", 3, 0)]
+    [TestCase("Body.ObjectArray[i].Array[3]", 0, 3)]
+    [TestCase("Body.ObjectArray[i].Array[j]", 0, 3)]
+    public void GetCorrectObject_InvalidPath_ThrowsResultSetException(string key, int indexI = 0, int indexJ = 0)
     {
         // Act
-        Action act = () => ResultSetHelper.GetCorrectObject<JValue>(key, null, resultSet);
+        Action act = () => ResultSetHelper.GetCorrectObject<JValue>(key, [indexI, indexJ], resultSet);
 
         // Assert
-        act.Should().Throw<Exception>("because the path is invalid.");
+        act.Should().Throw<ResultSetException>("because the path is invalid.");
     }
 }
