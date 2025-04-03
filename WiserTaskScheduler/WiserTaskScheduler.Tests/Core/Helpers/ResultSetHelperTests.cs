09b73ea29dd36d1ea1ecc80642c6f0c180432995
MarkVanDijkHG <89684687+MarkVanDijkHG@users.noreply.github.com>
2025-04-03 15:54:18 +0200

    Refactor unit tests

--- WiserTaskScheduler/WiserTaskScheduler.Tests/Core/Helpers/ResultSetHelperTests.cs
+++ WiserTaskScheduler/WiserTaskScheduler.Tests/Core/Helpers/ResultSetHelperTests.cs
@@ -1,66 +1,50 @@
-﻿using FluentAssertions;
-using Newtonsoft.Json.Linq;
-using WiserTaskScheduler.Core.Helpers;
+﻿using WiserTaskScheduler.Core.Helpers;
 using WiserTaskScheduler.Core.Models;
 
 namespace WiserTaskScheduler.Tests.Core.Helpers;
 
+[Category("Core")]
 public class ResultSetHelperTests
 {
-    private JObject resultSet;
-
-    [OneTimeSetUp]
-    public void Setup()
+    private readonly JObject resultSet = new JObject
     {
-        resultSet = new JObject
+        ["Value"] = "Test1",
+        ["Body"] = new JObject
         {
-            ["Value"] = "Test1",
-            ["Body"] = new JObject
+            ["Value"] = "Test2",
+            ["Array"] = new JArray
             {
-                ["Value"] = "Test2",
-                ["Array"] = new JArray
-                {
-                    new JValue("Index0"),
-                    new JValue("Index1"),
-                    new JValue("Index2")
-                },
-                ["ObjectArray"] = new JArray
-                {
-                    new JObject
-                    {
-                        ["Value"] = "Test3",
-                        ["Array"] = new JArray
-                        {
-                            new JValue("InnerArrayIndex0"),
-                            new JValue("InnerArrayIndex1"),
-                            new JValue("InnerArrayIndex2")
-                        }
-                    },
-                    new JObject
-                    {
-                        ["Value"] = "Test4"
-                    }
-                }
+                new JValue("Index0"),
+                new JValue("Index1"),
+                new JValue("Index2")
             },
-            ["RawBody"] = "<html><body><h1>Test</h1></body></html>"
-        };
-    }
+            ["ObjectArray"] = new JArray
+            {
+                new JObject
+                {
+                    ["Value"] = "Test3",
+                    ["Array"] = new JArray
+                    {
+                        new JValue("InnerArrayIndex0"),
+                        new JValue("InnerArrayIndex1"),
+                        new JValue("InnerArrayIndex2")
+                    }
+                },
+                new JObject
+                {
+                    ["Value"] = "Test4"
+                }
+            }
+        },
+        ["RawBody"] = "<html><body><h1>Test</h1></body></html>"
+    };
 
     [Test]
-    [TestCase("Value", "Test1")]
-    [TestCase("Body.Value", "Test2")]
-    [TestCase("Body.Array[0]", "Index0")]
-    [TestCase("Body.Array[1]", "Index1")]
-    [TestCase("Body.Array[2]", "Index2")]
-    [TestCase("Body.Array[i]", "Index1", 1, 0)]
-    [TestCase("Body.ObjectArray[i].Value", "Test3", 0, 0)]
-    [TestCase("Body.ObjectArray[i].Array[0]", "InnerArrayIndex0", 0, 0)]
-    [TestCase("Body.ObjectArray[i].Array[j]", "InnerArrayIndex1", 0, 1)]
-    [TestCase("RawBody", "<html><body><h1>Test</h1></body></html>")]
-    public void GetCorrectObject_ValidPath_ReturnsCorrectObject(string key, string expectedValue, int indexI = 0, int indexJ = 0)
+    [TestCaseSource(typeof(TestCases), nameof(TestCases.GetCorrectObject_ValidPath_ReturnsCorrectObject_TestCases))]
+    public void GetCorrectObject_ValidPath_ReturnsCorrectObject(string key, List<int> rows, string expectedValue)
     {
         // Act
-        var actual = ResultSetHelper.GetCorrectObject<JValue>(key, [indexI, indexJ], resultSet);
+        var actual = ResultSetHelper.GetCorrectObject<JValue>(key, rows, resultSet);
 
         // Assert
         actual.Should<JValue>().NotBeNull("because we are only testing values present in the result set.");
@@ -68,9 +52,7 @@ public class ResultSetHelperTests
     }
 
     [Test]
-    [TestCase("")]
-    [TestCase("Invalid")]
-    [TestCase("Body.Invalid")]
+    [TestCaseSource(typeof(TestCases), nameof(TestCases.GetCorrectObject_InvalidPath_ReturnsNull_TestCases))]
     public void GetCorrectObject_InvalidPath_ReturnsNull(string key)
     {
         // Act
@@ -81,18 +63,57 @@ public class ResultSetHelperTests
     }
 
     [Test]
-    [TestCase("Invalid.Value")]
-    [TestCase("Body.Invalid.Value")]
-    [TestCase("Body.Array[3]")]
-    [TestCase("Body.Array[i]", 3, 0)]
-    [TestCase("Body.ObjectArray[i].Array[3]", 0, 3)]
-    [TestCase("Body.ObjectArray[i].Array[j]", 0, 3)]
-    public void GetCorrectObject_InvalidPath_ThrowsResultSetException(string key, int indexI = 0, int indexJ = 0)
+    [TestCaseSource(typeof(TestCases), nameof(TestCases.GetCorrectObject_InvalidPath_ThrowsResultSetException_TestCases))]
+    public void GetCorrectObject_InvalidPath_ThrowsResultSetException(string key, List<int> rows)
     {
         // Act
-        var actual = () => ResultSetHelper.GetCorrectObject<JValue>(key, [indexI, indexJ], resultSet);
+        var actual = () => ResultSetHelper.GetCorrectObject<JValue>(key, rows, resultSet);
 
         // Assert
         actual.Should().Throw<ResultSetException>("because the path is invalid.");
+    }
+
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    private class TestCases
+    {
+        public static IEnumerable<TestCaseData> GetCorrectObject_ValidPath_ReturnsCorrectObject_TestCases
+        {
+            get
+            {
+                yield return new TestCaseData("Value", new List<int> { 0, 0 }, "Test1");
+                yield return new TestCaseData("Body.Value", new List<int> { 0, 0 }, "Test2");
+                yield return new TestCaseData("Body.Array[0]", new List<int> { 0, 0 }, "Index0");
+                yield return new TestCaseData("Body.Array[1]", new List<int> { 0, 0 }, "Index1");
+                yield return new TestCaseData("Body.Array[2]", new List<int> { 0, 0 }, "Index2");
+                yield return new TestCaseData("Body.Array[i]", new List<int> { 1, 0 }, "Index1");
+                yield return new TestCaseData("Body.ObjectArray[i].Value", new List<int> { 0, 0 }, "Test3");
+                yield return new TestCaseData("Body.ObjectArray[i].Array[0]", new List<int> { 0, 0 }, "InnerArrayIndex0");
+                yield return new TestCaseData("Body.ObjectArray[i].Array[j]", new List<int> { 0, 1 }, "InnerArrayIndex1");
+                yield return new TestCaseData("RawBody", new List<int> { 0, 0 }, "<html><body><h1>Test</h1></body></html>");
+            }
+        }
+
+        public static IEnumerable<TestCaseData> GetCorrectObject_InvalidPath_ReturnsNull_TestCases
+        {
+            get
+            {
+                yield return new TestCaseData("");
+                yield return new TestCaseData("Invalid");
+                yield return new TestCaseData("Body.Invalid");
+            }
+        }
+
+        public static IEnumerable<TestCaseData> GetCorrectObject_InvalidPath_ThrowsResultSetException_TestCases
+        {
+            get
+            {
+                yield return new TestCaseData("Invalid.Value", new List<int> { 0, 0 });
+                yield return new TestCaseData("Body.Invalid.Value", new List<int> { 0, 0 });
+                yield return new TestCaseData("Body.Array[3]", new List<int> { 0, 0 });
+                yield return new TestCaseData("Body.Array[i]", new List<int> { 3, 0 });
+                yield return new TestCaseData("Body.ObjectArray[i].Array[3]", new List<int> { 0, 0 });
+                yield return new TestCaseData("Body.ObjectArray[i].Array[j]", new List<int> { 0, 3 });
+            }
+        }
     }
 }
