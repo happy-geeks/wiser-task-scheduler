e4ffe22089856f7cf26b48799105044b4c37b722
MarkVanDijkHG <89684687+MarkVanDijkHG@users.noreply.github.com>
2025-04-03 15:54:18 +0200

    Refactor unit tests

--- WiserTaskScheduler/WiserTaskScheduler.Tests/Core/Helpers/ReplacementHelperTests.cs
+++ WiserTaskScheduler/WiserTaskScheduler.Tests/Core/Helpers/ReplacementHelperTests.cs
@@ -1,72 +1,46 @@
-using DocumentFormat.OpenXml.Office2013.PowerPoint.Roaming;
-using FluentAssertions;
-using GeeksCoreLibrary.Core.Models;
-using Newtonsoft.Json.Linq;
 using WiserTaskScheduler.Core.Helpers;
 using WiserTaskScheduler.Core.Models;
 
 namespace WiserTaskScheduler.Tests.Core.Helpers;
 
+[Category("Core")]
 public class ReplacementHelperTests
 {
-    private static IEnumerable<TestCaseData> TestCases
+    private readonly JObject resultSet = new JObject
     {
-        get
+        ["Value"] = "Test1",
+        ["Body"] = new JObject
         {
-            yield return new TestCaseData("Value", "Test1", new List<int> { 0, 0 }, false);
-            yield return new TestCaseData("Body.Value", "Test2", new List<int> { 0, 0 }, false);
-            yield return new TestCaseData("Body.Array[0]", "Index0", new List<int> { 0, 0 }, false);
-            yield return new TestCaseData("Body.Array[1]", "Index1", new List<int> { 0, 0 }, false);
-            yield return new TestCaseData("Body.Array[2]", "Index2", new List<int> { 0, 0 }, false);
-            yield return new TestCaseData("Body.Array[i]", "Index1", new List<int> { 1, 0 }, false);
-            yield return new TestCaseData("Body.ObjectArray[i].Value", "Test3", new List<int> { 0, 0 }, false);
-            yield return new TestCaseData("Body.ObjectArray[i].Array[0]", "InnerArrayIndex0", new List<int> { 0, 0 }, false);
-            yield return new TestCaseData("Body.ObjectArray[i].Array[j]", "InnerArrayIndex1", new List<int> { 0, 1 }, false);
-            yield return new TestCaseData("RawBody", "&lt;html&gt;&lt;body&gt;&lt;h1&gt;Test&lt;/h1&gt;&lt;/body&gt;&lt;/html&gt;", new List<int> { 0, 0 }, true);
-        }
-    }
-
-    private JObject resultSet;
-
-    [OneTimeSetUp]
-    public void Setup()
-    {
-        resultSet = new JObject
-        {
-            ["Value"] = "Test1",
-            ["Body"] = new JObject
+            ["Value"] = "Test2",
+            ["Array"] = new JArray
             {
-                ["Value"] = "Test2",
-                ["Array"] = new JArray
-                {
-                    new JValue("Index0"),
-                    new JValue("Index1"),
-                    new JValue("Index2")
-                },
-                ["ObjectArray"] = new JArray
-                {
-                    new JObject
-                    {
-                        ["Value"] = "Test3",
-                        ["Array"] = new JArray
-                        {
-                            new JValue("InnerArrayIndex0"),
-                            new JValue("InnerArrayIndex1"),
-                            new JValue("InnerArrayIndex2")
-                        }
-                    },
-                    new JObject
-                    {
-                        ["Value"] = "Test4"
-                    }
-                }
+                new JValue("Index0"),
+                new JValue("Index1"),
+                new JValue("Index2")
             },
-            ["RawBody"] = "<html><body><h1>Test</h1></body></html>"
-        };
-    }
+            ["ObjectArray"] = new JArray
+            {
+                new JObject
+                {
+                    ["Value"] = "Test3",
+                    ["Array"] = new JArray
+                    {
+                        new JValue("InnerArrayIndex0"),
+                        new JValue("InnerArrayIndex1"),
+                        new JValue("InnerArrayIndex2")
+                    }
+                },
+                new JObject
+                {
+                    ["Value"] = "Test4"
+                }
+            }
+        },
+        ["RawBody"] = "<html><body><h1>Test</h1></body></html>"
+    };
 
-    [TestCaseSource(nameof(TestCases))]
-    public void GetValue_ValidPath_ReturnsCorrectValue(string key, string expectedValue, List<int> rows, bool htmlEncode)
+    [TestCaseSource(typeof(TestCases), nameof(TestCases.GetValue_ValidPath_ReturnsCorrectValue_TestCases))]
+    public void GetValue_ValidPath_ReturnsCorrectValue(string key, List<int> rows, bool htmlEncode, string expectedValue)
     {
         // Act
         var actual = ReplacementHelper.GetValue(key, rows, resultSet, htmlEncode);
@@ -77,9 +51,7 @@ public class ReplacementHelperTests
     }
 
     [Test]
-    [TestCase("Invalid?", "")]
-    [TestCase("Invalid?Not found", "Not found")]
-    [TestCase("Body.Array[3]?DBNull", "DBNull")]
+    [TestCaseSource(typeof(TestCases), nameof(TestCases.GetValue_InvalidPath_ReturnsDefaultValue_TestCases))]
     public void GetValue_InvalidPath_ReturnsDefaultValue(string key, string expectedValue)
     {
         // Act
@@ -91,18 +63,58 @@ public class ReplacementHelperTests
     }
 
     [Test]
-    [TestCase("Invalid.Value")]
-    [TestCase("Body.Invalid.Value")]
-    [TestCase("Body.Array[3]")]
-    [TestCase("Body.Array[i]", 3, 0)]
-    [TestCase("Body.ObjectArray[i].Array[3]", 0, 3)]
-    [TestCase("Body.ObjectArray[i].Array[j]", 0, 3)]
-    public void GetValue_InvalidPath_ThrowsException(string key, int indexI = 0, int indexJ = 0)
+    [TestCaseSource(typeof(TestCases), nameof(TestCases.GetValue_InvalidPath_ThrowsException_TestCases))]
+    public void GetValue_InvalidPath_ThrowsException(string key, List<int> rows)
     {
         // Act
-        var actual = () => ReplacementHelper.GetValue(key, null, resultSet, false);
+        var actual = () => ReplacementHelper.GetValue(key, rows, resultSet, false);
 
         // Assert
         actual.Should().Throw<ResultSetException>("because the path is invalid.");
+    }
+
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    private class TestCases
+    {
+        public static IEnumerable<TestCaseData> GetValue_ValidPath_ReturnsCorrectValue_TestCases
+        {
+            get
+            {
+                yield return new TestCaseData("Value", new List<int> { 0, 0 }, false, "Test1");
+                yield return new TestCaseData("Body.Value", new List<int> { 0, 0 }, false, "Test2");
+                yield return new TestCaseData("Body.Array[0]", new List<int> { 0, 0 }, false, "Index0");
+                yield return new TestCaseData("Body.Array[1]", new List<int> { 0, 0 }, false, "Index1");
+                yield return new TestCaseData("Body.Array[2]", new List<int> { 0, 0 }, false, "Index2");
+                yield return new TestCaseData("Body.Array[i]", new List<int> { 1, 0 }, false, "Index1");
+                yield return new TestCaseData("Body.ObjectArray[i].Value", new List<int> { 0, 0 }, false, "Test3");
+                yield return new TestCaseData("Body.ObjectArray[i].Array[0]", new List<int> { 0, 0 }, false, "InnerArrayIndex0");
+                yield return new TestCaseData("Body.ObjectArray[i].Array[j]", new List<int> { 0, 1 }, false, "InnerArrayIndex1");
+                yield return new TestCaseData("RawBody", new List<int> { 0, 0 }, false, "<html><body><h1>Test</h1></body></html>");
+                yield return new TestCaseData("RawBody", new List<int> { 0, 0 }, true, "&lt;html&gt;&lt;body&gt;&lt;h1&gt;Test&lt;/h1&gt;&lt;/body&gt;&lt;/html&gt;");
+            }
+        }
+
+        public static IEnumerable<TestCaseData> GetValue_InvalidPath_ReturnsDefaultValue_TestCases
+        {
+            get
+            {
+                yield return new TestCaseData("Invalid?", "");
+                yield return new TestCaseData("Invalid?Not found", "Not found");
+                yield return new TestCaseData("Body.Array[3]?DBNull", "DBNull");
+            }
+        }
+
+        public static IEnumerable<TestCaseData> GetValue_InvalidPath_ThrowsException_TestCases
+        {
+            get
+            {
+                yield return new TestCaseData("Invalid.Value", new List<int> { 0, 0 });
+                yield return new TestCaseData("Body.Invalid.Value", new List<int> { 0, 0 });
+                yield return new TestCaseData("Body.Array[3]", new List<int> { 0, 0 });
+                yield return new TestCaseData("Body.Array[i]", new List<int> { 3, 0 });
+                yield return new TestCaseData("Body.ObjectArray[i].Array[3]", new List<int> { 0, 0 });
+                yield return new TestCaseData("Body.ObjectArray[i].Array[j]", new List<int> { 0, 3 });
+            }
+        }
     }
 }
