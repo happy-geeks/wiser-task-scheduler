585e8551ba4472ea38a4db64942aa50db762c99f
MarkVanDijkHG <89684687+MarkVanDijkHG@users.noreply.github.com>
2025-06-10 15:14:47 +0200

    Add unit test setup for queries service execute method

---
+++ WiserTaskScheduler/WiserTaskScheduler.Tests/Mocks/MockLogger.cs
@@ -0,0 +1,21 @@
+using Microsoft.Extensions.Logging;
+
+namespace WiserTaskScheduler.Tests.Mocks;
+
+public class MockLogger<T> : ILogger<T>
+{
+    public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+
+    }
+}
