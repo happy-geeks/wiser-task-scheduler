2485fe6eca05cab0611d704570d504ecf44a27f1
Mike van Mourik <130973326+MikeVanMourik@users.noreply.github.com>
2025-08-28 14:13:01 +0200

    add products api update worker

---
+++ WiserTaskScheduler/WiserTaskScheduler/Core/Interfaces/IProductsApiUpdateService.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using WiserTaskScheduler.Core.Models;
+
+namespace WiserTaskScheduler.Core.Interfaces;
+
+/// <summary>
+/// A service to cleanup after the application.
+/// </summary>
+public interface IProductsApiUpdateService
+{
+    /// <summary>
+    /// Gets or sets the log settings that the Main service needs to use.
+    /// </summary>
+    LogSettings LogSettings { get; set; }
+
+    /// <summary>
+    /// Update the api responses of the products.
+    /// </summary>
+    /// <returns></returns>
+    Task UpdateProductsAsync();
+}
---
+++ WiserTaskScheduler/WiserTaskScheduler/Core/Models/ProductsApiUpdater/ProductsApiUpdateServiceSettings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using WiserTaskScheduler.Core.Workers;
+using WiserTaskScheduler.Modules.RunSchemes.Enums;
+using WiserTaskScheduler.Modules.RunSchemes.Models;
+
+namespace WiserTaskScheduler.Core.Models.ProductsApiUpdater;
+
+public class ProductsApiUpdateServiceSettings
+{
+    /// <summary>
+    ///  Gets or sets a value indicating whether the <see cref="ProductsApiUpdateWorker"/> is enabled.
+    /// </summary>
+    public bool Enabled { get; set; } = false;
+
+    /// <summary>
+    /// Gets or Sets the url of the products api.
+    /// </summary>
+    public string ProductsApiUrl { get; set; } = "https://wiserapi.configuratoren.nl/api/v3/products";
+
+    /// <summary>
+    /// Gets or sets the run scheme for the <see cref="ProductsApiUpdateWorker"/>.
+    /// </summary>
+    public RunSchemeModel RunScheme { get; set; } = new()
+    {
+        Type = RunSchemeTypes.Continuous,
+        Delay = TimeSpan.FromMinutes(5),
+        StartTime = TimeSpan.FromHours(4),
+        StopTime = TimeSpan.FromMinutes((5 * 60) + 30)
+    };
+}
